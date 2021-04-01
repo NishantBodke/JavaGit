2f1b2b34ef11f82084380e5f0df3785f531adc6c
Nishant <Nishant@DESKTOP-T3RCKH3>
2021-04-01 12:20:22 -0700

    First Git Project

---
+++ GitHub/src/Github1.java
@@ -0,0 +1,39 @@
+
+public class Github1 {
+	
+
+		public String CName;
+		public int CNumber;
+		public int Quantity;
+		public double UnitPrice;
+		public double TotalPrice;
+		public String getCName() {
+			return CName;
+		}
+		public void setCName(String cName) {
+			CName = cName;
+		}
+		public int getCNumber() {
+			return CNumber;
+		}
+		public void setCNumber(int cNumber) {
+			CNumber = cNumber;
+		}
+		public int getQuantity() {
+			return Quantity;
+		}
+		public void setQuantity(int quantity) {
+			Quantity = quantity;
+		}
+		public double getUnitPrice() {
+			return UnitPrice;
+		}
+		public void setUnitPrice(double unitPrice) {
+			UnitPrice = unitPrice;
+		}
+		
+		
+		
+	}
+
+ 
